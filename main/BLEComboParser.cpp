679e636a21b83c840767ef81cf99afaa8949ccb0
earth <earthicko@gmail.com>
2023-03-22 22:49:14 +0900

    Feat: 마우스 휠/움직임 보정값, 휠 업데이트 주기 설정

--- main/BLEComboParser.cpp
+++ main/BLEComboParser.cpp
@@ -13,18 +13,25 @@ size_t BLEComboParser::parseHIDDataKeyboard(int8_t* buf)
 
 size_t BLEComboParser::parseHIDDataMouse(int8_t* buf)
 {
+    static int8_t wheel_update_counter;
+
     if (buf[0] == 1) {
         int8_t m[5];
         if (buf[1] & MOUSE_MIDDLE && (buf[2] || buf[3])) {
-            m[0] = buf[1]; // button state
+            wheel_update_counter++;
+            if (wheel_update_counter != BLECOMBOPARSER_SCROLL_UPDATE_FREQ)
+                return (0);
+            m[0] = buf[1] & ~((int8_t)MOUSE_MIDDLE); // button state
             m[1] = 0; // dx
             m[2] = 0; // dy
-            m[3] = -buf[3] * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // vertical wheel = dy
-            m[4] = buf[2] * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // horizontal wheel = dx
+            m[3] = ((int)(-buf[3])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // vertical wheel = dy
+            m[4] = ((int)(buf[2])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // horizontal wheel = dx
+            if (wheel_update_counter == BLECOMBOPARSER_SCROLL_UPDATE_FREQ)
+                wheel_update_counter = 0;
         } else {
             m[0] = buf[1]; // button state
-            m[1] = buf[2]; // dx
-            m[2] = buf[3]; // dy
+            m[1] = ((int)(buf[2])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dx
+            m[2] = ((int)(buf[3])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dy
             m[3] = 0; // vertical wheel
             m[4] = 0; // horizontal wheel
         }
