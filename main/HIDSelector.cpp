1575e733af3bc58291353f41e81e7cf4497435c8
earth <earthicko@gmail.com>
2023-03-23 22:22:20 +0900

    Refactor: static 변수 호이스팅

--- main/HIDSelector.cpp
+++ main/HIDSelector.cpp
@@ -14,6 +14,12 @@ bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto)
 // Will be called for all HID data received from the USB interface
 void HIDSelector::ParseHIDData(USBHID* hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t* buf)
 {
+    static const parser_t parsers[] = {
+        (const parser_t)(NULL),
+        (const parser_t)(&BLEComboParser::parseHIDDataKeyboard),
+        (const parser_t)(&BLEComboParser::parseHIDDataMouse),
+    };
+
     DEBUG_PRINTF("HID %p, ep %d, is rpt %d, len %d, buf {", hid, ep, is_rpt_id, len);
     for (uint8_t i = 0; i < len; i++)
         DEBUG_PRINTF("%d, ", (int8_t)buf[i]);
@@ -21,11 +27,6 @@ void HIDSelector::ParseHIDData(USBHID* hid, uint8_t ep, bool is_rpt_id, uint8_t 
     DEBUG_PRINTF("\n");
     if (!(_ble->isConnected()))
         return;
-    static const parser_t parsers[] = {
-        (const parser_t)(NULL),
-        (const parser_t)(&BLEComboParser::parseHIDDataKeyboard),
-        (const parser_t)(&BLEComboParser::parseHIDDataMouse),
-    };
     (_ble->*parsers[ep])((int8_t*)buf);
     if (ep == 1) {
         if (*((uint64_t*)buf) == 0) {
