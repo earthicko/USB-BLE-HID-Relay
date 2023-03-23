8d6307c0bd3bc0c5a788f91801e7497652d8e2b7
earth <earthicko@gmail.com>
2023-03-23 21:11:38 +0900

    Feat: 가운데 버튼의 행동 로직 분기

--- main/BLEComboParser.cpp
+++ main/BLEComboParser.cpp
@@ -1,4 +1,5 @@
 #include "BLEComboParser.h"
+#include "DebugPrint.h"
 
 BLEComboParser::BLEComboParser(std::string deviceName, std::string deviceManufacturer, uint8_t batteryLevel)
     : BLECombo(deviceName, deviceManufacturer, batteryLevel)
@@ -11,30 +12,80 @@ size_t BLEComboParser::parseHIDDataKeyboard(int8_t* buf)
     return (1);
 }
 
+static bool should_update_scroll(void)
+{
+    static int counter;
+
+    counter++;
+    if (counter != BLECOMBOPARSER_SCROLL_UPDATE_FREQ)
+        return (false);
+    counter = 0;
+    return (true);
+}
+
 size_t BLEComboParser::parseHIDDataMouse(int8_t* buf)
 {
-    static int8_t wheel_update_counter;
+    static bool prev_middle_stat;
+    static bool did_trigger_wheel_movement;
 
     if (buf[0] == 1) {
+        static const int8_t illegal_vals[] = { 40, -40, 56, -56, 24, -24 };
+        for (int8_t i = 2; i <= 3; i++) {
+            for (int8_t j = 0; j < sizeof(illegal_vals) / sizeof(int8_t); j++) {
+                if (buf[i] == illegal_vals[j])
+                    return (0);
+            }
+        }
+
         int8_t m[5];
-        if (buf[1] & MOUSE_MIDDLE && (buf[2] || buf[3])) {
-            wheel_update_counter++;
-            if (wheel_update_counter != BLECOMBOPARSER_SCROLL_UPDATE_FREQ)
+        if (buf[1] & MOUSE_MIDDLE) {
+            // middle button stays being pressed
+            if (should_update_scroll()) {
+                // update scroll movement
+                m[0] = buf[1] & ~((int8_t)MOUSE_MIDDLE); // button state
+                m[1] = 0; // dx
+                m[2] = 0; // dy
+                m[3] = ((int)(-buf[3])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // vertical wheel = dy
+                m[4] = ((int)(buf[2])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // horizontal wheel = dx
+                did_trigger_wheel_movement = true;
+            } else {
+                // ignore scroll movement
+                m[0] = buf[1] & ~((int8_t)MOUSE_MIDDLE); // button state
+                m[1] = 0; // dx
+                m[2] = 0; // dy
+                m[3] = 0;
+                m[4] = 0;
+            }
+        } else if (!(buf[1] & MOUSE_MIDDLE) && (prev_middle_stat)) {
+            // middle button just being released
+            if (did_trigger_wheel_movement) {
+                // middle button was pressed to scroll
+                m[0] = buf[1]; // button state
+                m[1] = ((int)(buf[2])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dx
+                m[2] = ((int)(buf[3])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dy
+                m[3] = 0; // vertical wheel
+                m[4] = 0; // horizontal wheel
+                did_trigger_wheel_movement = false;
+            } else {
+                // middle button was pressed to act as wheel click
+                mouseClick(MOUSE_MIDDLE);
                 return (0);
-            m[0] = buf[1] & ~((int8_t)MOUSE_MIDDLE); // button state
-            m[1] = 0; // dx
-            m[2] = 0; // dy
-            m[3] = ((int)(-buf[3])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // vertical wheel = dy
-            m[4] = ((int)(buf[2])) * BLECOMBOPARSER_SCROLL_MULTIPLIER / BLECOMBOPARSER_SCROLL_DIVIDER; // horizontal wheel = dx
-            if (wheel_update_counter == BLECOMBOPARSER_SCROLL_UPDATE_FREQ)
-                wheel_update_counter = 0;
+            }
         } else {
+            // middle button stays being released
             m[0] = buf[1]; // button state
             m[1] = ((int)(buf[2])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dx
             m[2] = ((int)(buf[3])) * BLECOMBOPARSER_MOVE_MULTIPLIER / BLECOMBOPARSER_MOVE_DIVIDER; // dy
             m[3] = 0; // vertical wheel
             m[4] = 0; // horizontal wheel
         }
+        prev_middle_stat = buf[1] & MOUSE_MIDDLE;
+
+        DEBUG_PRINTF("m = {");
+        for (int i = 0; i < 5; i++)
+            DEBUG_PRINTF("%d, ", m[i]);
+        DEBUG_PRINTF("}\n");
+
         mouseNotify(sizeof(m), (uint8_t*)m);
     } else if (buf[0] == 3) {
         if (buf[1] == 0 && buf[2] == 0 && buf[3] == 0) { // release
