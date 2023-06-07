1fc8ec3195cacf1d01c6dd9c71c56124eb144344
Donghyeon Lee <earthicko@gmail.com>
2023-06-07 16:23:29 +0900

    Fix: Revert pull from branch "dualcore"

--- main/DebugPrint.h
+++ main/DebugPrint.h
@@ -1,8 +1,8 @@
 #ifndef DEBUGPRINT_H
 #define DEBUGPRINT_H
 
-#include <Arduino.h>
 #include "SerialPrintf.h"
+#include <Arduino.h>
 
 // #define _DEBUG
 
--- main/HIDSelector.h
+++ main/HIDSelector.h
@@ -1,21 +1,21 @@
 #ifndef HIDSELECTOR_H
 #define HIDSELECTOR_H
 
-#include "BLEComboParser.h"
+#include "MsgPipe.h"
 #include <hidcomposite.h>
 #include <usbhub.h>
 
 // Override HIDComposite to be able to select which interface we want to hook into
 class HIDSelector : public HIDComposite {
 public:
-    HIDSelector(USB* p, BLEComboParser* ble);
+    HIDSelector(USB* p, MsgPipe<hidmsg_t>* hidpipe);
 
 protected:
     void ParseHIDData(USBHID* hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t* buf); // Called by the HIDComposite library
     bool SelectInterface(uint8_t iface, uint8_t proto);
 
 private:
-    BLEComboParser* _ble;
+    MsgPipe<hidmsg_t>* _hidpipe;
 };
 
 #endif
